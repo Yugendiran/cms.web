46e4e27524ce446d6e9ab0af476f70ae2a44887d
Yugendiran <yyugi64@gmail.com>
2024-02-24 13:17:07 +0530

    .

---
+++ components/NavBar.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <nav class="bg-gray-800">
+      <div class="px-2 mx-auto sm:px-6 lg:px-8">
+        <div class="relative flex items-center justify-between h-20">
+          <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <!-- Mobile menu button-->
+            <div
+              class="flex items-center justify-center w-10 h-10 text-xl text-white"
+            >
+              <i
+                v-if="mobileMenu"
+                @click="mobileMenu = false"
+                class="fa-solid fa-xmark"
+              ></i>
+              <i v-else @click="mobileMenu = true" class="fa-solid fa-bars"></i>
+            </div>
+          </div>
+          <div
+            class="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start"
+          >
+            <div class="flex items-center flex-shrink-0">
+              <img
+                class="w-auto h-8"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                alt="Your Company"
+              />
+            </div>
+            <div class="hidden sm:ml-6 sm:block">
+              <div class="flex space-x-4">
+                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                <NuxtLink
+                  v-for="(item, index) in menuItems"
+                  :key="index"
+                  :to="item.route"
+                  class="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
+                >
+                  {{ item.title }}
+                </NuxtLink>
+              </div>
+            </div>
+          </div>
+          <div
+            class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+          >
+            <!-- Profile dropdown -->
+            <div class="relative ml-3">
+              <div>
+                <div
+                  class="relative flex text-sm bg-gray-800 rounded-full group focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                >
+                  <span class="absolute -inset-1.5"></span>
+                  <span class="sr-only">Open user menu</span>
+                  <img
+                    class="w-8 h-8 rounded-full"
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+
+                  <div
+                    class="absolute right-0 z-10 pt-3 origin-top-right top-full"
+                  >
+                    <div
+                      class="hidden w-48 py-1 bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none group-hover:block"
+                      role="menu"
+                      aria-orientation="vertical"
+                      aria-labelledby="user-menu-button"
+                      tabindex="-1"
+                    >
+                      <NuxtLink
+                        to="/profile"
+                        class="block px-4 py-2 text-sm text-gray-700"
+                      >
+                        Profile
+                      </NuxtLink>
+                      <NuxtLink
+                        to="/logout"
+                        class="block px-4 py-2 text-sm text-gray-700"
+                      >
+                        Sign out
+                      </NuxtLink>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Mobile menu, show/hide based on menu state. -->
+      <div v-if="mobileMenu" class="sm:hidden" id="mobile-menu">
+        <div class="px-2 pt-2 pb-3 space-y-1">
+          <NuxtLink
+            v-for="(item, index) in menuItems"
+            :key="index"
+            :to="item.route"
+            class="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
+          >
+            {{ item.title }}
+          </NuxtLink>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mobileMenu: false,
+      profileIsOpen: false,
+      menuItems: [
+        {
+          title: "Home",
+          route: "/",
+        },
+        {
+          title: "All blogs",
+          route: "/blogs",
+        },
+      ],
+    };
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,25 +5,131 @@ definePageMeta({
 </script>
 
 <template>
-  <div>Home {{ message }}</div>
+  <div>
+    <NavBar />
+    <div class="flex flex-col w-full gap-5 lg:h-[calc(100vh-80px)] lg:flex-row">
+      <div class="w-full h-full lg:w-[70%] lg:overflow-auto p-10">
+        <div class="space-y-20 lg:space-y-20">
+          <article
+            v-for="n in 5"
+            :key="n"
+            class="relative flex flex-col gap-8 isolate lg:flex-row"
+          >
+            <div
+              class="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0"
+            >
+              <img
+                src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80"
+                alt=""
+                class="absolute inset-0 object-cover w-full h-full rounded-2xl bg-gray-50"
+              />
+              <div
+                class="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10"
+              ></div>
+            </div>
+            <div>
+              <div class="flex items-center text-xs gap-x-4">
+                <time datetime="2020-03-16" class="text-gray-500"
+                  >Mar 16, 2020</time
+                >
+                <a
+                  href="#"
+                  class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                  >Marketing</a
+                >
+              </div>
+              <div class="relative max-w-xl group">
+                <h3
+                  class="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600"
+                >
+                  <a href="#">
+                    <span class="absolute inset-0"></span>
+                    Boost your conversion rate
+                  </a>
+                </h3>
+                <p class="mt-5 text-sm leading-6 text-gray-600">
+                  Illo sint voluptas. Error voluptates culpa eligendi. Hic vel
+                  totam vitae illo. Non aliquid explicabo necessitatibus unde.
+                  Sed exercitationem placeat consectetur nulla deserunt vel
+                  iusto corrupti dicta laboris incididunt.
+                </p>
+              </div>
+              <div class="flex pt-6 mt-6 border-t border-gray-900/5">
+                <div class="relative flex items-center gap-x-4">
+                  <img
+                    src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                    class="w-10 h-10 rounded-full bg-gray-50"
+                  />
+                  <div class="text-sm leading-6">
+                    <p class="font-semibold text-gray-900">
+                      <a href="#">
+                        <span class="absolute inset-0"></span>
+                        Michael Foster
+                      </a>
+                    </p>
+                    <p class="text-gray-600">Co-Founder / CTO</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </article>
+
+          <!-- More posts... -->
+        </div>
+      </div>
+      <div class="w-full h-full lg:w-[30%] lg:overflow-auto p-10">
+        <div>
+          <h2 class="text-2xl font-semibold text-gray-900">Popular Posts</h2>
+          <div class="mt-6 space-y-6">
+            <div v-for="n in 5" :key="n" class="flex items-center space-x-4">
+              <div class="relative w-20 h-20">
+                <img
+                  src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80"
+                  alt=""
+                  class="absolute inset-0 object-cover w-full h-full rounded-lg bg-gray-50"
+                />
+                <div
+                  class="absolute inset-0 rounded-lg ring-1 ring-inset ring-gray-900/10"
+                ></div>
+              </div>
+              <div class="flex-1">
+                <h3 class="text-lg font-semibold leading-6 text-gray-900">
+                  Boost your conversion rate
+                </h3>
+                <div class="flex justify-between text-xs">
+                  <p>Read more</p>
+                  <p>Mar 16, 2020</p>
+                </div>
+              </div>
+            </div>
+            <!-- More posts... -->
+          </div>
+        </div>
+
+        <div class="mt-10">
+          <h2 class="text-2xl font-semibold text-gray-900">Tags</h2>
+          <div class="flex flex-wrap gap-3 mt-6">
+            <div
+              v-for="n in 10"
+              :key="n"
+              class="relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+            >
+              Marketing
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      message: {},
-    };
+    return {};
   },
-  mounted() {
-    this.testApi();
-  },
-  methods: {
-    testApi() {
-      this.$api.get("/user/test-p").then((res) => {
-        this.message = res;
-      });
-    },
-  },
+  mounted() {},
+  methods: {},
 };
 </script>
